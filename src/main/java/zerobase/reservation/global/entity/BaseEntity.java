6f7420157cf1b2752a564c0923b59bf9f4057382
Sang Hwa Sung <sangwha0@naver.com>
2024-10-06 23:56:48 +0900

    six

--- src/main/java/zerobase/reservation/global/entity/BaseEntity.java
+++ src/main/java/zerobase/reservation/global/entity/BaseEntity.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+// 전역 엔티티
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
